317d98cdbd920190136267b8deb61bd60896aa4e
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-11-08 18:53:00 +0100

    Updated Lab 2

--- Lab 2/Eaa2rotMat.m
+++ Lab 2/Eaa2rotMat.m
@@ -1,18 +1,17 @@
-function [rotationMatrix] = Eaa2rotMat (rotationAxis,angle)
+function [rotationMatrix] = Eaa2rotMat (raxis,angle)
 %This method generates the rotation matrix that rotates around that axis a
 %certain angle
 %INFO: Angle must be in degrees
-if (iscolumn(rotationAxis) == 0)
-    rotationAxis = rotationAxis';
+if (iscolumn(raxis) == 0)
+    raxis = raxis';
 end
-rotationAxis.normalize();
-
+raxis = raxis / sqrt(raxis(1) * raxis(1) + raxis(2) * raxis(2) + raxis(3) * raxis(3));
 
 %Create stoichiometry matrix
-stoichiometryMatrix = [ 0, -rotationAxis(3), rotationAxis(2);
-                        rotationAxis(3), 0, -rotationAxis(1);
-                        -rotationAxis(2), rotationAxis(1), 0];
+stoichiometryMatrix = [ 0, -raxis(3), raxis(2);
+                        raxis(3), 0, -raxis(1);
+                        -raxis(2), raxis(1), 0];
 
-rotationMatrix = eye(3)*cosd(angle) + (1-cosd(angle)) * (rotationAxis * rotationAxis') + sind(angle)*stoichiometryMatrix;
+rotationMatrix = eye(3)*cosd(angle) + (1-cosd(angle)) * (raxis * raxis') + sind(angle)*stoichiometryMatrix;
 
 end
