0349048108a2aa62ac9d6e504eedf39d5837d63b
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-11-20 18:57:56 +0100

    Added test ex 5

---
+++ Lab 2/eAngles2rotM.m
@@ -0,0 +1,8 @@
+function [rMatrix] = eAngles2rotM(anglePhi, angleTheta, anglePsi)
+%eAngles2rotM given a set of euler angles phi, theta and psi, return its respective rotation matrix.
+rMatrix1 = Eaa2rotMat([1;0;0], anglePhi);
+rMatrix2 = Eaa2rotMat([0;1;0], angleTheta);
+rMatrix3 = Eaa2rotMat([0;0;1], anglePsi);
+rMatrix = rMatrix3'*rMatrix2'*rMatrix1';
+end
+
