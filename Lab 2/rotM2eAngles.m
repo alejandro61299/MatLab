3267a83942cbc89cb54579b39f57f568473c7918
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-11-21 20:10:26 +0100

    Function not working

---
+++ Lab 2/rotM2eAngles.m
@@ -0,0 +1,11 @@
+function [anglePhi, angleTheta, anglePsi] = rotM2eAngles(rMat)
+%rotM2eAngles: Gets a rotation matrix and returns the euler angles
+anglePsi = atan2d(-rMat(3:1), rMat(3:3));
+% The 3:3 element is multiplied by cos(phi), we would divide it to get the
+% value of cos(psi), but cos(0) = 1, so it's not necessary
+anglePhi = 0;
+yPart = (rMat(2:1))/cosd(anglePsi);
+xPart = (rMat(1:1))/cosd(anglePsi);
+angleTheta = atan2d(yPart,xPart);
+end
+
