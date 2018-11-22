1343bc7c23b62d6a1fcd7d0a50ff5163a484e65c
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-11-22 21:54:36 +0100

    Saving just in case

--- Lab 2/rotM2eAngles.m
+++ Lab 2/rotM2eAngles.m
@@ -5,20 +5,23 @@ function [anglePhi, angleTheta, anglePsi, flag] = rotM2eAngles(rMat)
 %   = -1 sin(angleTheta) = 1
 %   =  0 sin(angleTheta) ~= 1 | -1
 
+ %angleTheta(2)= asind(-rMat(3,1));
+ %angleTheta(1) = -angleTheta(2);
  angleTheta(1)= asind(-rMat(3,1));
- angleTheta(2) = 180 - angleTheta;
+ angleTheta(2) = angleTheta(1) + 180;
 
-if -rMat(3,1)== 1
+if rMat(3,1) == 1
     flag = -1;
     anglePhi(1) = 0;
     anglePhi(2) = 0;
-    
-    
-elseif -rMat(3,1) == -1
+    anglePsi(1) = anglePhi(1) - acosd(rMat(2,2));
+    anglePsi(2) = 180 - anglePsi(1);
+elseif rMat(3,1) == -1
     flag = 1;
     anglePhi(1) = 0;
     anglePhi(2) = 0;
-    
+    anglePsi(1) = acosd(rMat(2,2)) - anglePhi(1); 
+    anglePsi(2) = 180 - anglePsi(1);
 else
     flag = 0;
     anglePsi(1) = atan2d(rMat(2,1)/cosd(angleTheta(1)), rMat(1,1)/cosd(angleTheta(1)));
