1683ca446508ae753ea08b70b268d2ff454ec375
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-12-22 14:59:36 +0100

    Added rotVec

--- Lab 4/rotVbyq.m
+++ Lab 4/rotVbyq.m
@@ -4,12 +4,12 @@ function [vr] = rotVbyq(v, q)
 %   q = quaternion to rotate
 %   vr = vector rotated
 
-v_q = [ 0 ; v(1), v(2), v(3)];
-q_c = [ q(1) , - q(2) ,- q(3) , -q(4)];
+v_q = [ 0 ; v(1); v(2); v(3)];
+q_c = [ q(1) ; - q(2) ;- q(3) ; -q(4)];
 v_q_r = MultiplyQuat(q , v_q);
 v_q_r = MultiplyQuat(v_q_r , q_c);
 
-vr = [ v_q_r(2), v_q_r(3), v_q_r(4)];
+vr = [ v_q_r(2); v_q_r(3); v_q_r(4)];
 
 end
 
